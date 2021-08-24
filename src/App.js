ef841d17e640fe46d525f0186d91fcae8e3cac00
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-24 19:56:43 +0600

    Componernt tree

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import React from "react";
+import MainComponent from "./components/MainComponent";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MainComponent></MainComponent>
+    </div>
+  );
 }
 
 export default App;
