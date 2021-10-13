5923745014475827ffd392703475b3ae160274cc
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-14 01:52:06 +0600

    Prepare Project for router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import React from "react";
 import MainComponent from "./components/MainComponent";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <MainComponent></MainComponent>
+      <BrowserRouter>
+        <MainComponent></MainComponent>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Contact from "./Contact";
+import Home from "./Home";
 import Menu from "./Menu";
 
 const Body = () => {
   return (
     <div>
       <Menu></Menu>
+      <Home></Home>
+      <Contact></Contact>
+      <Body></Body>
     </div>
   );
 };
