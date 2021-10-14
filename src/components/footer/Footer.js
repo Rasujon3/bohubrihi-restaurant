61e89d554fd765695776b984a0780a30adf2982d
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-14 22:25:46 +0600

    footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react';
 
 const Footer = () => {
-  return <div>{/* <h1>This is footer</h1> */}</div>;
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-auto">
+            <p>Fatafati Restaurant &#169; Copyright {new Date().getFullYear()} </p>
+          </div>
+        </div>
+      </div>
+    </div >
+  );
 };
 
 export default Footer;
