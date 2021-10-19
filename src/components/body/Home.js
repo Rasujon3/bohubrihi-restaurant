04fae13894fbcba16b88c787b703b570e08ddd31
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-20 01:19:21 +0600

    Using fontAwesome sponner

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Loading from './Loading';
 
 
 
@@ -10,6 +11,7 @@ class Home extends Component {
         return (
             <div>
                 <h1>Fatafati Restaurant</h1>
+                <Loading />
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
