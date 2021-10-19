5082f7513442514e46698d9f0a7ca9da4df41214
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-20 00:50:38 +0600

    Using action creators
    Module 20, Video 1

--- src/components/body/About.js
+++ src/components/body/About.js
@@ -5,7 +5,18 @@ const About = () => {
 
     return (
         <div>
-            <h1>About</h1>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora omnis, quas quaerat ut qui vero aut consequatur molestias! Omnis modi soluta quo perspiciatis architecto! Ipsa dolorem earum aliquam adipisci tempore nulla exercitationem et, praesentium maiores. Perspiciatis cum ratione nam, eum, ipsum deserunt ex inventore perferendis recusandae obcaecati modi adipisci. Ipsa!</p>
+            </div>
         </div>
     );
 };
